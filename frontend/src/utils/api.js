80119bd53b498453d6fcd314ee312e3c5bd48ee2
Bz2127 <bezawitgetachew23@gmail.com>
2026-03-16 13:01:15 +0300

    Update API baseURL to use environment variables

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+ 
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
 
-// Automatically attach token to every request
-api.interceptors.request.use((config) => {
 
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -15,7 +15,6 @@ api.interceptors.request.use((config) => {
   }
 
   return config;
-
 });
 
 export default api;
