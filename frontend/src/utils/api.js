b1f1945c033983c0dcb821271c664f1496eec0ec
Bz2127 <bezawitgetachew23@gmail.com>
2026-03-16 13:52:02 +0300

    Fix: Final API routing for Render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
+const isLive = window.location.hostname.includes("render.com");
+
 const api = axios.create({
- 
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: isLive 
+    ? "https://ecommerce-backend-t7o6.onrender.com/api" 
+    : "/api", 
   withCredentials: true,
 });
 
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
